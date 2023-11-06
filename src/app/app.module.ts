d682fd1336b7b3bd1577a654b44e0966d06a9425
dbc2201 <divyanshb22@gmail.com>
2023-11-06 13:52:38 +0530

    generated home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
 	declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { FooterComponent } from './footer/footer.component';
 		AppRoutingModule,
 		HeaderComponent,
 		FooterComponent,
+		HomeComponent,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
